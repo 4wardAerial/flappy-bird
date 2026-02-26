30ebb658110bab2baaa840c8e5408c56d7436a59
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-02-26 13:30:07 -0300

    Fixes bug where player sprite and colors would be lost when going back to menu

--- main/scripts/start_menu.gd
+++ main/scripts/start_menu.gd
@@ -2,12 +2,16 @@ extends Control
 
 @onready var player_sprite : Sprite2D = $HBoxContainer/VBoxContainer/PlayerContainer/PlayerSprite
 @onready var color : int = Global.current_color
+@onready var sprite_frame : int = Global.current_sprite
 
-var x_frame : int = 0
+func _ready() -> void:
+	player_sprite.frame_coords = Vector2i(sprite_frame, 4)
+	player_sprite.self_modulate = Global.player_colors[color]	
+
 
 func _on_start_button_pressed() -> void:
 	Global.current_color = color
-	Global.current_sprite = x_frame
+	Global.current_sprite = sprite_frame
 	get_tree().change_scene_to_file("res://main/scenes/game.tscn")
 
 
@@ -26,10 +30,10 @@ func _on_color_button_down_pressed() -> void:
 
 
 func _on_player_button_right_pressed() -> void:
-	x_frame = posmod(x_frame + 1, 4)
-	player_sprite.frame_coords = Vector2i(x_frame, 3)
+	sprite_frame = posmod(sprite_frame + 1, 4)
+	player_sprite.frame_coords = Vector2i(sprite_frame, 3)
 
 
 func _on_player_button_left_pressed() -> void:
-	x_frame = posmod(x_frame - 1, 4)
-	player_sprite.frame_coords = Vector2i(x_frame, 3)
+	sprite_frame = posmod(sprite_frame - 1, 4)
+	player_sprite.frame_coords = Vector2i(sprite_frame, 3)

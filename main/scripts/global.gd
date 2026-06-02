1772bc21ae339a9fbce5f4e8c8e6cd9b785bf572
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-06-02 12:06:22 -0300

    json high scores

--- main/scripts/global.gd
+++ main/scripts/global.gd
@@ -3,18 +3,23 @@ extends Node
 var current_type : int = 0
 var current_color : int = 0
 var current_sprite : int = 0
-
-
 var player_types : Dictionary[int, float] = {
 	0 : -300,
 	1 : -175,
 	2 : -450,
 }
-
-
 var player_colors : Dictionary[int, Color] = {
 	0 : Color(1.0, 1.0, 1.0, 1.0),
 	1 : Color(0.0, 0.0, 1.0, 1.0),
 	2 : Color(0.0, 1.0, 0.0, 1.0),
 	3 : Color(1.0, 0.0, 0.0, 1.0),
 }
+
+const HIGH_SCORES : String = "res://main/files/high_scores.json"
+
+func _ready():
+	load_highscores()
+
+func load_highscores() -> void:
+	var json_text = FileAccess.get_file_as_string(HIGH_SCORES)
+	var json_dict = JSON.parse_string(json_text)
